22eb7f7419058873be1d614d67e1b289ed966f82
Keith Franklin <keith30895@gmail.com>
2019-04-01 23:11:36 +0800

    fixed codebeat identified issue.

--- blogREST/common/Exception.py
+++ blogREST/common/Exception.py
@@ -1,5 +1,5 @@
 class ValidationException(Exception):
-    def __init__(self, message='Validation error', error_field_name='unknown_field', *args, **kwargs):
-        super().__init__(args, **kwargs)
+    def __init__(self, message='Validation error', error_field_name='unknown_field'):
+        super().__init__()
         self.error_field_name = error_field_name
         self.message = message
